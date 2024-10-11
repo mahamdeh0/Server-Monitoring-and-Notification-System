1e0617e7383e8d37e17387640c3ff04235b25396
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-11 14:32:11 +0300

    Define IMessageQueue interface for message queuing with Publish method

---
+++ Server Statistics Collection Service/Interfaces/IMessageQueue.cs
@@ -0,0 +1,9 @@
+﻿using Server_Statistics_Collection_Service.Models;
+
+namespace Server_Statistics_Collection_Service.Interfaces
+{
+    public interface IMessageQueue
+    {
+        void Publish(string topic, ServerStatistics statistics);
+    }
+}
