03f2fdab54908ff8b008384d5a082498ea2dd08d
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-12 18:10:10 +0300

    Implement Main program to initialize SignalR client and handle alerts

---
+++ SignalR Event Consumer Service/Program.cs
@@ -0,0 +1,24 @@
+﻿using SignalR_Event_Consumer_Service;
+using SignalR_Event_Consumer_Service.Config;
+using SignalR_Event_Consumer_Service.Interfaces;
+
+namespace SignalRAlertConsumer;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var configHelper = new ConfigurationHelper();
+        string signalRHubUrl = configHelper.GetSignalRHubUrl();
+
+        ISignalRClient signalRClient = new SignalRClient(signalRHubUrl);
+        signalRClient.Start();
+
+        signalRClient.RegisterAlertHandler(alert =>
+        {
+            Console.WriteLine(alert);
+        });
+
+        Console.ReadLine();
+    }
+}
