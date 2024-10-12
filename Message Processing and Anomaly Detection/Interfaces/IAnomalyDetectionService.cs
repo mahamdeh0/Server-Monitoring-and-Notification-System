2e3029fa98b4a9c444157dd1e003e2de27e92068
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-12 11:50:00 +0300

    Add IAnomalyDetectionService interface for anomaly detection logic

---
+++ Message Processing and Anomaly Detection/Interfaces/IAnomalyDetectionService.cs
@@ -0,0 +1,12 @@
+﻿using Message_Processing_and_Anomaly_Detection.Models;
+
+namespace Message_Processing_and_Anomaly_Detection.Interfaces
+{
+    public interface IAnomalyDetectionService
+    {
+        public bool CheckForMemoryAnomaly(ServerStatistics current, ServerStatistics previous);
+        public bool CheckForCpuAnomaly(ServerStatistics current, ServerStatistics previous);
+        public bool CheckForHighMemoryUsage(ServerStatistics statistics);
+        public bool CheckForHighCpuUsage(ServerStatistics statistics);
+    }
+}
