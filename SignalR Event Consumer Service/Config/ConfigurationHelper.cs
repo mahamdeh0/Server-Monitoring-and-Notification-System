d98fe37b9de45f71c8d85d9952b552d3bc9f888f
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-12 18:09:41 +0300

    Implement ConfigurationHelper class to manage application configuration and retrieve SignalR Hub URL

---
+++ SignalR Event Consumer Service/Config/ConfigurationHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SignalR_Event_Consumer_Service.Config
+{
+    public class ConfigurationHelper
+    {
+        private readonly IConfigurationRoot _configuration;
+
+        public ConfigurationHelper()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            _configuration = builder.Build();
+        }
+
+        public string GetSignalRHubUrl()
+        {
+            return _configuration.GetConnectionString("SignalRUrl");
+        }
+    }
+}
