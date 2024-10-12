03304239687b43d6b611bf3725b06d3e18e17fb3
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-12 10:03:54 +0300

    Define ServerStatistics class for server statistics data model

---
+++ Message Processing and Anomaly Detection/Models/ServerStatistics.cs
@@ -0,0 +1,11 @@
+﻿namespace Message_Processing_and_Anomaly_Detection.Models
+{
+    public class ServerStatistics
+    {
+        public string ServerIdentifier { get; set; }
+        public double MemoryUsage { get; set; } // in MB
+        public double AvailableMemory { get; set; } // in MB
+        public double CpuUsage { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
