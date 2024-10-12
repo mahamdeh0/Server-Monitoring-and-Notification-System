a6689a3083ba1f2f9968e988d430a7abfe59c85a
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-12 12:55:19 +0300

    Implement SignalRService for sending alerts via SignalR

---
+++ Message Processing and Anomaly Detection/Services/SignalRService.cs
@@ -0,0 +1,23 @@
+﻿using Message_Processing_and_Anomaly_Detection.Interfaces;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Message_Processing_and_Anomaly_Detection.Services
+{
+    public class SignalRService : ISignalRService
+    {
+        private readonly HubConnection _connection;
+
+        public SignalRService(string signalRUrl)
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl(signalRUrl)
+                .Build();
+            _connection.StartAsync().Wait();
+        }
+
+        public void SendAlert(string message)
+        {
+            _connection.InvokeAsync("SendAlert", message);
+        }
+    }
+}
