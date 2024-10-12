eabac5aab9a6d009a19d44d7788128993d5fd21c
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-12 18:10:02 +0300

    Implement SignalRClient class to connect to SignalR Hub and handle alerts

---
+++ SignalR Event Consumer Service/SignalRClient.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SignalR_Event_Consumer_Service.Interfaces;
+
+namespace SignalR_Event_Consumer_Service
+{
+    public class SignalRClient : ISignalRClient
+    {
+        private readonly HubConnection _connection;
+
+        public SignalRClient(string hubUrl)
+        {
+            _connection = new HubConnectionBuilder()
+                .WithUrl(hubUrl)
+                .Build();
+        }
+
+        public void Start()
+        {
+            try
+            {
+                _connection.StartAsync().Wait();
+                Console.WriteLine("SignalR connected.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"SignalR connection error: {ex.Message}");
+            }
+        }
+
+        public void RegisterAlertHandler(Action<string> handler)
+        {
+            _connection.On("ReceiveAlert", handler);
+        }
+    }
+}
